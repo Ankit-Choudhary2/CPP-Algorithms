ab5a5f169d9b22aa090acb273aeb23ca43ace683
Ankit Choudhary <ankitjeet224@gmail.com>
2026-05-28 08:59:37 +0530

    add edge case handling and time & space complexity comments

--- Sorting/Merge_sort.cpp
+++ Sorting/Merge_sort.cpp
@@ -1,59 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge_both(vector <int> &vec, int low, int mid, int high ) {
+void merge_both(vector<int> &vec, int low, int mid, int high)
+{
     int left = low;
     int right = mid + 1;
-    vector <int> temp;
-    while(left <= mid && right <= high) {
-        if(vec[left] <= vec[right]) {
-            temp.push_back(vec[left]);
+    vector<int> temp;
+    temp.reserve(high - low + 1); // pre sets the size of to save memory
+    // merge both halves in sorted order
+    while (left <= mid && right <= high)
+    {
+        if (vec[left] <= vec[right])
+        {
+            temp.emplace_back(vec[left]);
             left++;
         }
-        else {
-            temp.push_back(vec[right]);
+        else
+        {
+            temp.emplace_back(vec[right]);
             right++;
         }
-        
-    } 
-    while(left <= mid) {
-        temp.push_back(vec[left]);
-            left++;
     }
-    while(right <= high) {
-        temp.push_back(vec[right]);
-            right++;
+    // copy remaining elements
+    while (left <= mid)
+    {
+        temp.emplace_back(vec[left]);
+        left++;
     }
-    
-    for(int i = low; i <= high; i++) {
-        vec[i] = temp[i-low];
+    while (right <= high)
+    {
+        temp.emplace_back(vec[right]);
+        right++;
+    }
+    // write back to original
+    for (int indx = low; indx <= high; indx++)
+    {
+        vec[indx] = temp[indx - low];
     }
 }
-void merge_sort(vector <int> &vec, int low, int high) {
-    int mid = (low + high)/2;
-    if(low == high) return;
+void merge_sort(vector<int> &vec, int low, int high)
+{
+    if (low >= high)
+        return;
+    int mid = (low + high) / 2;
     merge_sort(vec, low, mid);
-    merge_sort(vec, mid +1, high);
+    merge_sort(vec, mid + 1, high);
     merge_both(vec, low, mid, high);
 }
 
-int main() {
-    // freopen("D:\\CPP\\CPP-Algorithms\\input.txt", "r", stdin);
-    // freopen("D:\\CPP\\output.txt", "w", stdout);
+int main()
+{
     int n;
     cin >> n;
-    vector <int> vec;
-    for(int i = 0; i < n; i++) {
+    vector<int> vec;
+    for (int i = 0; i < n; i++)
+    {
         int ch;
         cin >> ch;
-        vec.push_back(ch);
+        vec.emplace_back(ch);
     }
+    if (vec.empty())
+        return 0;
+
     int low = 0;
-    int high = n-1;
+    int high = n - 1;
     merge_sort(vec, low, high);
-    
-    for(int ch :vec) {
+
+    for (int ch : vec)
+    {
         cout << ch << " ";
     }
-
 }
+
+/*
+TIME COMPLEXITY::
+    Best : O(nlogn)
+    Avg  : O(nlogn)
+    Worst: O(nlogn)
+SPACE COMPLEXITY ::
+    Overall    : O(n)
+    Call stack : O(logn)
+*/
--- Sorting/Quick_sort.cpp
+++ Sorting/Quick_sort.cpp
@@ -1,44 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int partition(vector<int>& vec, int low, int high) {
+int partition(vector<int> &vec, int low, int high)
+{
     int pivot = vec[low];
     int i = low;
     int j = high;
-    while(i < j) {
-        while(vec[i] <= pivot && i < high) {
+    while (i < j)
+    {
+        while (vec[i] <= pivot && i < high)
+        {
             i++;
         }
-        while(vec[j] > pivot && j > low) {
+        while (vec[j] > pivot && j > low)
+        {
             j--;
         }
-        if(i < j) {
+        if (i < j)
+        {
             swap(vec[i], vec[j]);
         }
     }
     swap(vec[low], vec[j]);
     return j;
 }
-void qs(vector<int>& vec, int low, int high) {
-    if(low > high) return;
+void qs(vector<int> &vec, int low, int high)
+{
+    if (low > high)
+        return;
     int pIndex = partition(vec, low, high);
     qs(vec, low, pIndex - 1);
     qs(vec, pIndex + 1, high);
 }
-void quick_sort(vector<int>& vec) {
+void quick_sort(vector<int> &vec)
+{
     int low = 0;
     int high = vec.size() - 1;
     qs(vec, low, high);
 }
-int main() {
-    // freopen("D:\\CPP\\input.txt", "r", stdin);
-    vector <int> vec;
+int main()
+{
+    vector<int> vec;
     int x;
-    while(cin >> x) {
-        vec.push_back(x);
+    while (cin >> x)
+    {
+        vec.emplace_back(x);
     }
+    if (vec.empty())
+        return 0;
     quick_sort(vec);
-    for(int x : vec) {
-    cout << x << " ";
+    for (int x : vec)
+    {
+        cout << x << " ";
+    }
 }
-}
+
+/*
+TIME COMPLEXITY::
+    Best : O(nlogn)
+    Avg  : O(nlogn)
+    Worst: O(n²)
+SPACE COMPLEXITY ::
+    Auxillary    : O(1)
+    Call stack : O(logn)
+*/
