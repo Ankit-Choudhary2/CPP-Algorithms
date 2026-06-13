7bd6700cfc4b0662e9678dc3af575202f2f02716
Ankit Choudhary <ankitjeet224@gmail.com>
2026-06-13 12:26:16 +0530

    add left rotate array by k

---
+++ Arrays/Easy/Left_rotate_array_by_k.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Left_rotate_by_k(vector<int>& vec, int k) {
+    int n = vec.size();
+
+    vector<int> temp(k);
+    for(int i = 0; i < k; i++) {
+        temp[i] = vec[i];
+    }
+
+    for(int i = k; i < n; i++) {
+        vec[i-k] = vec[i];
+    }
+    for(int i = n-k; i < n; i++) {
+        vec[i] = temp[i-(n-k)];
+    }
+    
+}
+int main() {    
+    vector<int> vec;
+    int k;
+    cin >> k;
+    int x;
+    while(cin >> x) vec.emplace_back(x);
+    Left_rotate_by_k(vec,k);
+
+    for(int x : vec) cout << x << " ";
+    cout << endl;
+}
